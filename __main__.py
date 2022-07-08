ce962dd96168edfb771cef657ca3d8fb7bbb54e4
Your Name <leonardongc@gmail.com>
2022-07-08 09:18:17 -0300

    add some buttons to control the board

--- __main__.py
+++ __main__.py
@@ -1,29 +1,26 @@
-
 from LifeGame import *
 from tkinter import *
 
-
-gameLines = 30
-gameColumns = 30
+gameLines = 10
+gameColumns = 10
 boardHeight = 600
 boardWidth = 600
 boardBackground = "white"
 cellColor = "black"
 cellHeight = boardHeight/gameLines
 cellWidth = boardWidth/gameColumns
-frameTime = 300
+frameTime = 500
 
 root = Tk()
 root.title("Game Of Life")
 root.geometry('800x640')
-
 
 class Board(Canvas):
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
         self.boardState = LifeGame(gameLines,gameColumns,initializer="RANDOM")
         self.printState()
-        self.updateState()
+        self.running = 0
 
     def printState(self):
         self.delete("all")
@@ -32,19 +29,39 @@ class Board(Canvas):
                 if self.boardState.matrix[line][column] > 0:
                     x0,y0,x1,y1 = column*cellWidth, line*cellHeight, (column + 1)*cellWidth, (line + 1)*cellHeight
                     self.create_rectangle(x0,y0,x1,y1,fill=cellColor)
-    
+
     def updateState(self):
         self.boardState = self.boardState.nextState()
         self.printState()
-        self.after(frameTime,self.updateState)
+        if self.running: self.after(frameTime,self.updateState)
 
+    def resetBoard(self,event):
+        self.boardState = LifeGame(gameLines,gameColumns,initializer="RANDOM")
+        self.printState()
+        if self.running: self.updateState()
 
+    def killBoard(self,event):
+        self.boardState = LifeGame(gameLines,gameColumns,initializer="EMPTY")
+        self.printState()
+        if self.running: self.updateState()
 
-
-
+    def stoprun(self,event):
+        self.running = self.running ^ 1
+        if self.running: self.updateState()
+        
+commandBar = Frame(root)
+toggleButton = Button(commandBar,text = "RUN - STOP")
+resetButton = Button(commandBar, text = "RESET")
+killButton = Button(commandBar, text = "KILL")
 canvas = Board(root, height = boardHeight, width = boardWidth,bg = boardBackground)
 
-canvas.pack()
-
+toggleButton.pack()
+toggleButton.bind('<Button-1>', canvas.stoprun)
+resetButton.pack()
+resetButton.bind('<Button-1>', canvas.resetBoard)
+killButton.pack()
+killButton.bind('<Button-1>', canvas.killBoard)
+commandBar.grid()
+canvas.grid(column = 3, row = 0)
 
 root.mainloop()
